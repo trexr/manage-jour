9ebcfa8630bd9c3ae68313fbeb5eda668ab9d92a
trexr <admin@Trevors-MacBook-Pro.local>
2020-02-08 21:45:47 -0800

    Inits Skeleton for Manajer

---
+++ src/components/layout/SignedInLinks.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/"> New Project </NavLink>
+      </li>
+      <li>
+        <NavLink to="/"> Log out </NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating pink lighten-1">
+          TS
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
